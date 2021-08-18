9c12c4a1625c2dd7b54ef3afa112aceb144ba040
kenbun <takefumic@gmail.com>
2021-08-19 01:02:45 +0900

    connect panda

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request
 import ku_panda as ku
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -16,7 +17,19 @@ def login():
 def assign_list():
   print(request.data)
   login, session = ku.login(request.form["username"], request.form["password"])
-  return render_template('assign_list.html', status=login)
+  status = login_successful(login)
+  if status == 0:
+    return render_template('assign_list.html', status=status)
+  else:
+    return redirect(url_for('login'))
+
+def login_successful(html):
+  bs = BeautifulSoup(html.text, 'html.parser')
+  title = bs.find('title').text[0:5]
+  if title = "PandA":
+    return 0
+  else:
+    return 1
 
 if __name__ == '__main__':
     app.run(debug=True)
