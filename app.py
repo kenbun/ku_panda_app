468011ffb573960af23e1fa7e9b13a59b6aa4131
kenbun <takefumic@gmail.com>
2021-11-10 20:57:27 +0900

    delete view dead_assign, add radio checked

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ def assign_list():
     subject = ku.get_subject(login, semester)
     subject = ku.get_url(subject,session,loop)
     assign = ku.get_yet_assign(subject, session, loop)
-    yet_assign, dead_assign = assign_classification(assign)
-    return render_template('assign_list.html', yet_list=yet_assign, dead_list=dead_assign, test_list=ku.get_yet_test(subject, session, loop))
+    yet_assign = assign_classification(assign)
+    return render_template('assign_list.html', yet_list=yet_assign, test_list=ku.get_yet_test(subject, session, loop))
   else:
     return redirect(url_for('/'))
 
@@ -45,9 +45,9 @@ def assign_classification(assign):
     deadline = re.split('[/: ]', var.deadline)
     if now < datetime.datetime(*[int(num) for num in deadline]):
       yet_assign = yet_assign.append({'subject':assign.iloc[index].subject, 'title':assign.iloc[index].title, 'deadline':assign.iloc[index].deadline, 'status':assign.iloc[index].status, 'url':assign.iloc[index].url}, ignore_index=True)
-    else:
-      dead_assign = dead_assign.append({'subject':assign.iloc[index].subject, 'title':assign.iloc[index].title, 'deadline':assign.iloc[index].deadline, 'status':assign.iloc[index].status, 'url':assign.iloc[index].url}, ignore_index=True)
-  return yet_assign, dead_assign
+    # else:
+    #   dead_assign = dead_assign.append({'subject':assign.iloc[index].subject, 'title':assign.iloc[index].title, 'deadline':assign.iloc[index].deadline, 'status':assign.iloc[index].status, 'url':assign.iloc[index].url}, ignore_index=True)
+  return yet_assign#, dead_assign
 
 if __name__ == '__main__':
   app.run(debug=True)
